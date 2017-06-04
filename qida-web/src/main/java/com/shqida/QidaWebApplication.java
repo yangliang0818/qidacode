cca7d473d485a4e425c701a1673d06a6c0608d9d
yangliang0818@126.com <yangliang0818@126.com>
2017-06-04 22:11:15 +0800

    提交人：杨亮

--- qida-web/src/main/java/com/shqida/QidaWebApplication.java
+++ qida-web/src/main/java/com/shqida/QidaWebApplication.java
@@ -4,6 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * @Copyright (C), 2012-2022 上海骑达科技 版权所有
+ * @Website www.shqida.com
+ * @Author: Created by YangLiang
+ * @Date: 2017/6/4
+ * @Version: 1.0.0
+ * @Description:
+ * @History: 变更记录
+ * <author>           <time>             <version>        <desc>
+ * YangLiang          2017/6/4            00000001         创建文件
+ */
 @SpringBootApplication
 public class QidaWebApplication {
 
