1096d96b8eb798089c7bda4f77ab436b63cb30c1
yangliang0818@126.com <yangliang0818@126.com>
2017-06-04 23:13:52 +0800

    提交人：杨亮

---
+++ qida-demo/src/main/java/com/shqida/demo/inf/IndexService.java
@@ -0,0 +1,16 @@
+package com.shqida.demo.inf;
+
+/**
+ * @Copyright (C), 2012-2022 上海骑达科技 版权所有
+ * @Website www.shqida.com
+ * @Author: Created by YangLiang
+ * @Date: 2017/6/4
+ * @Version: 1.0.0
+ * @Description:
+ * @History: 变更记录
+ * <author>           <time>             <version>        <desc>
+ * YangLiang          2017/6/4            00000001         创建文件
+ */
+public interface IndexService {
+    void sayHello();
+}
